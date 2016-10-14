f23407e78e200171f9d2d34829264be5a61c4cda
jesdiazs <jes.diazserrano@gmail.com>
2016-10-13 23:05:52 -0300

    actualizacion final del ejercicio de la leccion 20

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-// tu codigo va aca
+// Leccion 20  funcion callback parte 1
+
+function callbackPromedio (num1, num2, num3, callback){
+	var promedio= ((num1+num2+num3)/3); 
+	return callback(promedio);
+}
+
+
+// parte 2 segundero 
+
+var tiempo=1;
+function cambiarSegundero(){
+	document.getElementById("titulo").innerHTML= tiempo;
+	tiempo++;
+}
+
+function segundero(){
+	setInterval(cambiarSegundero, 1000);
+}
